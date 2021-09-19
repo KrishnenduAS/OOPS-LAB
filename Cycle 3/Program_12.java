c6086f241d98eeb9d8b9b2e92d7c9d0a4cad0889
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-09-19 22:13:42 +0530

    Update Program_12.java

--- Cycle 3/Program_12.java
+++ Cycle 3/Program_12.java
@@ -1,88 +1,93 @@
 import java.util.*;
-interface Shape
-{
-	void area();
-	void perimeter();
-}
-class Circle implements Shape
-{
-	float radius;
-	Circle(float r)
-	{
-		radius=r;
-	}
-	public void area()
-	{
-		System.out.println("\nAREA OF CIRCLE   = "+(3.14*radius*radius));
-	}
-	public void perimeter()
-	{
-		System.out.println("\nPERIMETER OF CIRCLE = "+(2*3.14*radius));
-	}
-}
-class Rectangle implements Shape
-{
-	float length,breadth;
-	Rectangle(float l,float b)
-	{
-		length=l;
-		breadth=b;
-	}
-	public void area()
-	{
-		System.out.println("\nAREA OF RECTANGLE ="+(length*breadth));
-	}
-	public void perimeter()
-	{
-		System.out.println("\nPERIMETER OF RECTANGLE="+(2*(length+breadth)));
-	}
-}
-class Program_12
-{
-	public static void main(String args[])
-	{
-		int ch,c1,c2;
-		Scanner sc=new Scanner(System.in);
-		do
-		{
-			System.out.println("\nMENU\n1.CIRCLE\n2.RECTANGLE\n3.EXIT");
-			System.out.println("\nEnter your choice:");
-			ch=Integer.parseInt(sc.nextLine());
-			switch(ch)
-			{
-				case 1:System.out.println("Enter Radius:");
-				       float r=Float.parseFloat(sc.nextLine());
-				       Shape c=new Circle(r);
-				       System.out.println("\n1.AREA\n2.PERIMETER");
-				       System.out.println("\nEnter your choice:");
-				       c1=Integer.parseInt(sc.nextLine());
-				       switch(c1)
-				       {
-					       case 1:c.area();
-						      break;
-					       case 2:c.perimeter();
-						      break;
-				       }
-				       break;
-				case 2:System.out.println("Enter length:");
-				       float l=Float.parseFloat(sc.nextLine());
-				       System.out.println("Enter Breadth:");           
-				       float b=Float.parseFloat(sc.nextLine());
-				       System.out.println("\n1.AREA\n2.PERIMETER");
-				       Shape s=new Rectangle(l,b);
+import java.lang.*;
 
-				       System.out.println("\nEnter your choice:");
-				       c2=Integer.parseInt(sc.nextLine());
-				       switch(c2)
-				       {
-					       case 1:s.area();
-						      break;
-					       case 2:s.perimeter();
-						      break;
-				       }
-				       break;
-				case 3:break;
-			}
-		}while(ch!=3);
-	}
+interface Shape {
+    float pi = 3.14F;
+    float area();
+    float perimeter();
+}
+
+class Circle implements Shape {
+    Scanner sc = new Scanner(System.in);
+    int r;
+
+    public float area() {
+        System.out.print("Enter the radius : ");
+        r = Integer.parseInt(sc.nextLine());
+        return (pi * r * r);
+
+    }
+
+    public float perimeter() {
+        System.out.print("Enter the radius : ");
+        r = Integer.parseInt(sc.nextLine());
+        return (2 * pi * r);
+    }
+
+}
+
+class Rectangle implements Shape {
+    Scanner sc = new Scanner(System.in);
+    int l, b;
+
+    public float area() {
+        System.out.print("Enter the Length : ");
+        l = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter the breadth : ");
+        b = Integer.parseInt(sc.nextLine());
+        return (l * b);
+
+    }
+
+    public float perimeter() {
+        System.out.print("Enter the Length : ");
+        l = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter the breadth : ");
+        b = Integer.parseInt(sc.nextLine());
+        return (2 * (l + b));
+
+    }
+}
+
+class Program_12 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Circle c = new Circle();
+        Rectangle r = new Rectangle();
+        int ch;
+        while (true) {
+            System.out.println("1:Area of Circle");
+            System.out.println("2:Perimeter of Circle");
+            System.out.println("3:Area of Rectangle");
+            System.out.println("4:Perimter of Rectangle");
+            System.out.println("5:EXIT");
+            System.out.println("enter choice ");
+            ch = Integer.parseInt(sc.nextLine());
+            switch (ch) {
+                case 1:
+                    float ar = c.area();
+                    System.out.println("Area  :" + ar);
+                    break;
+                case 2:
+                    float pr = c.perimeter();
+                    System.out.println(pr);
+                    break;
+                case 3:
+                    float a = r.area();
+                    System.out.println("Area :" + a);
+                    break;
+                case 4:
+                    float pr1 = r.perimeter();
+                    System.out.println(pr1);
+                    break;
+                case 5:
+                    System.out.println("Exiting the Program");
+                    System.exit(0);
+                default:
+                    System.out.println("invalid!");
+
+            }
+        }
+
+    }
 }
