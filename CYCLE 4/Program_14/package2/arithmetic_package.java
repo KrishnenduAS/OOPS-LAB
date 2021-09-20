470d2ef62dac6e52b5ec7ef565e3f0e3f0af82eb
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-09-20 19:11:44 +0530

    Create arithmetic_package.java

---
+++ CYCLE 4/Program_14/package2/arithmetic_package.java
@@ -0,0 +1,9 @@
+package arithmetic_package;
+
+interface interface_graphics{
+   public float add(int a, int b);
+   public float divide(int a, int b);
+   public float multiple(int a, int b);
+   public float substract(int a, int b);
+   public float remainder(int a, int b);
+}
