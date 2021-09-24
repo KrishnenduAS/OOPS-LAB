a885da15bef54b678366a303ecd09a4f8a84628f
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-09-24 14:18:24 +0530

    Update arithmetic_package.java

--- CYCLE 4/Program_14/package2/arithmetic_package.java
+++ CYCLE 4/Program_14/package2/arithmetic_package.java
@@ -1,11 +1,11 @@
 package package2;
 
 interface interface_graphics{
-   public float add(int a, int b);
-   public float divide(int a, int b);
-   public float multiple(int a, int b);
-   public float substract(int a, int b);
-   public float remainder(int a, int b);
+   float add(int a, int b);
+   float divide(int a, int b);
+   float multiple(int a, int b);
+   float substract(int a, int b);
+   float remainder(int a, int b);
 }
 
 public class arithmetic_package implements interface_graphics {
